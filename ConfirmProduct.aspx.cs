89419f106f06ac1f8111ad1d8d55b86437122d68
theTtay <3874262@clases.edu.sv>
2025-03-04 15:15:56 -0600

    SessionState - segundo commit y push

--- ConfirmProduct.aspx.cs
+++ ConfirmProduct.aspx.cs
@@ -11,7 +11,24 @@ namespace SessionState
 	{
 		protected void Page_Load(object sender, EventArgs e)
 		{
-			
+			//Retrieve the session variables
+			ddlCategory.SelectedValue = Session["ddlCategory"].ToString();
+			ddlSupplier.SelectedValue = Session["ddlSupplier"].ToString();
+			txtProduct.Text = Session["strProduct"].ToString();
+			txtDescription.Text = Session["strDescription"].ToString();
+			txtImage.Text = Session["strImage"].ToString();
+			Decimal decPrice = Convert.ToDecimal(Session["decPrice"]);
+			txtPrice.Text = decPrice.ToString("c");
+			txtNumberInStock.Text = Session["bytNumberInStock"].ToString();
+			txtNumberOnOrder.Text = Session["bytNumberOnOrder"].ToString();
+			txtReorderLevel.Text = Session["bytReorderLevel"].ToString();
+			//Compute and display the value in stock and the value on order 
+			Byte bytNumberInStock = Convert.ToByte(Session["bytNumberInStock"]);
+			Byte bytNumberOnOrder = Convert.ToByte(Session["bytNumberOnOrder"]);
+			Decimal decValueInStock = decPrice * bytNumberInStock;
+			Decimal decValueOnOrder = decPrice * bytNumberOnOrder;
+			txtNumberInStock.Text = decValueInStock.ToString("c");
+			txtNumberOnOrder.Text = decValueOnOrder.ToString("c");
 		}
 
 		protected void btnSave_Click(object sender, EventArgs e)
